de1ed27b40b0d5850fc53d64fd61d7ee45d9e0e1
thomas <thomas.bournaveas@gmail.com>
2020-07-16 18:10:06 +0300

    Added button functionality

--- app/src/main/java/com/thomas/appeggtimer/MainActivity.java
+++ app/src/main/java/com/thomas/appeggtimer/MainActivity.java
@@ -1,6 +1,9 @@
 package com.thomas.appeggtimer;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -27,13 +30,7 @@ public class MainActivity extends AppCompatActivity {
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-                int minutes = i / 60;
-                int seconds = i - minutes * 60;
-
-                String minutesString = getTimeAsString(minutes);
-                String secondString = getTimeAsString(seconds);
-
-                timeTextView.setText(minutesString + ":" + secondString);
+                updateTimer(i);
             }
 
             @Override
@@ -48,9 +45,44 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void buttonClicked(View view) {
+        // Take the number of seconds the user wants to set and multiply by 1000,
+        // so then we can work with milliseconds
+        new CountDownTimer(seekBar.getProgress() * 1000, 1000) {
+
+            @Override
+            public void onTick(long l) {
+                // Divide the number of milliseconds by 1000, so it turns to second
+                updateTimer((int) l / 1000);
+            }
+
+            @Override
+            public void onFinish() {
+                Log.i("Finished", "Timer all done");
+            }
+        }.start();
+    }
+
+    /**
+     * Updates timeTextView display.
+     *
+     * @param secondsLeft
+     */
+
+    public void updateTimer(int secondsLeft) {
+        int minutes = secondsLeft / 60;
+        int seconds = secondsLeft - minutes * 60;
+
+        String minutesString = getTimeAsString(minutes);
+        String secondString = getTimeAsString(seconds);
+
+        timeTextView.setText(minutesString + ":" + secondString);
+    }
+
     /**
      * Converts time to String and checks String object's length. If length is equal to 1, then adds
      * an extra zero prefix.
+     *
      * @param time
      * @return
      */
