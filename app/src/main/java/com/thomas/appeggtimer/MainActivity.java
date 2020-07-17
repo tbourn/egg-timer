dd525b900846f19fd30f6975d2f1ce5b1dcc39ad
thomas <thomas.bournaveas@gmail.com>
2020-07-17 12:34:21 +0300

    Documentation and Licence

--- app/src/main/java/com/thomas/appeggtimer/MainActivity.java
+++ app/src/main/java/com/thomas/appeggtimer/MainActivity.java
@@ -1,3 +1,27 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2020 Thomas Bournaveas
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 package com.thomas.appeggtimer;
 
 import android.graphics.Color;
@@ -52,7 +76,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * @param view
+     *  Once the button is pressed, the method checks if counter is active. If true, it resets the timer.
+     *  Otherwise, it activates the counter, disables the state of the seekBar and sets its visibility to invisible.
+     *  Also, it changes the goButton text and starts a new CountDownTimer, where inside onTick method
+     *  updateTimer method is called and inside onFinish method the media file is specified. Finally,
+     *  inside the onFinish method, it resets the timer and sets the seekBar's visibility to visible.
+     *
+     * @param view the view object
      */
     public void buttonClicked(View view) {
 
@@ -111,7 +141,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Updates timeTextView display.
      *
-     * @param secondsLeft
+     * @param secondsLeft the seconds
      */
     private void updateTimer(int secondsLeft) {
         String timeAsString = getTimerAsString(secondsLeft);
@@ -119,10 +149,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * Returns seconds as String representation in form of HH:MM:SS
+     * Returns seconds as String representation in the form of HH:MM:SS
      *
-     * @param secondsLeft
-     * @return
+     * @param secondsLeft the seconds
+     * @return the time as String in the form of HH:MM:SS
      */
     private String getTimerAsString(int secondsLeft) {
         int hours = secondsLeft / 3600;
@@ -151,8 +181,8 @@ public class MainActivity extends AppCompatActivity {
      * Converts time to String and checks String object's length. If length is equal to 1, then adds
      * an extra zero prefix.
      *
-     * @param time
-     * @return
+     * @param time the time in String format
+     * @return the time as String, prefixed with zeros if necessary
      */
     private String getPrefixedTime(int time) {
         String timeString = Integer.toString(time);
